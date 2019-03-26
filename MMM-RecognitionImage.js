12c146d347221e273f613afde9297b0dec269ab5
Luis Martinez <lmartinezruizit@gmail.com>
2019-03-26 13:05:20 +0100

    Second commit

--- MMM-RecognitionImage.js
+++ MMM-RecognitionImage.js
@@ -1,27 +1,16 @@
 Module.register('MMM-RecognitionImage', {
 
 	defaults: {
-		// Threshold for the confidence of a recognized face before it's considered a
-		// positive match.  Confidence values below this threshold will be considered
-		// a positive match because the lower the confidence value, or distance, the
-		// more confident the algorithm is that the face was correctly detected.
-		threshold: 50,
-		// force the use of a usb webcam on raspberry pi (on other platforms this is always true automatically)
+		// Threshold for the confidence of a recognized image
+		confidence_threshold: 5,
+		// USB camera = true, Pi camera = false
 		useUSBCam: false,
-		// Path to your training xml
-		trainingFile: 'modules/MMM-Facial-Recognition-OCV3/training.xml',
-		// recognition intervall in seconds (smaller number = faster but CPU intens!)
-		interval: 2,
-		// Logout delay after last recognition so that a user does not get instantly logged out if he turns away from the mirror for a few seconds
-		logoutDelay: 15,
 		// Array with usernames (copy and paste from training script)
 		users: [],
 		//Module set used for strangers and if no user is detected
 		defaultClass: "default",
 		//Set of modules which should be shown for every user
 		everyoneClass: "everyone",
-		// Boolean to toggle welcomeMessage
-		welcomeMessage: true
 	},
 
 	login_user: function () {
